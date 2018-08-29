827e3d100ec65ca9f320900579980edc19034538
aandroomeedaa <41657603+aandroomeedaa@users.noreply.github.com>
2018-08-29 15:11:07 +0700

    Update exercise11-balikankatakata.js

--- exercise11-balikankatakata.js
+++ exercise11-balikankatakata.js
@@ -1,7 +1,10 @@
-console.log('Logic Challenge - Balik Kata -aandroomeedaa');
+
 function balikKata(kata) {
-  // you can only write your code here!
-return kata.split('').reverse().join('');
+  var balkat=''
+for(var i =kata.length-1;i>=0; i--){
+ balkat = balkat + kata[i]
+}
+return balkat 
 }
 
 // TEST CASES
